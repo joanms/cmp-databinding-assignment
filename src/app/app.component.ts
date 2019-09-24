cde3f4d195a5fca59dbd7970c2c2297c157f95af
Joan Sweeney <joanms@eircom.net>
2019-09-24 15:25:59 +0100

    Add code to generate text for odd numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // When the event starts, the current number is output
+  // Odd and even numbers are defined as arrays
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  // When the event starts, the numbers are checked and pushed to the appropriate array - odd or even
   onIntervalStart(outputNumber: number) {
-    console.log(outputNumber);
+    if (outputNumber % 2 === 0) {
+      this.evenNumbers.push(outputNumber);
+    } else {
+      this.oddNumbers.push(outputNumber);
+    }
   }
 }
